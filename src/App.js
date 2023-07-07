6cb51954cfc61cc1763cfafc2a8660c1e476e452
fandri.yunus <fandri.yunus@gmail.com>
2023-07-07 14:32:45 +0800

    create navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import Topbar from "./components/topbar/Topbar";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Topbar />
+    </div>
+  );
 }
 
 export default App;
