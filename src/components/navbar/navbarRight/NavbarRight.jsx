e27799b774989eb64f0188a2b7524120c2a218a7
Fundream18 <fandriydb@gmail.com>
2023-07-09 17:58:25 +0800

    add ui navbar right

--- src/components/navbar/navbarRight/NavbarRight.jsx
+++ src/components/navbar/navbarRight/NavbarRight.jsx
@@ -1,5 +1,44 @@
-import React from "react";
+import {
+  Chat,
+  NotificationsNone,
+  QuestionAnswer,
+  QuestionAnswerRounded,
+  Search,
+  Window,
+  ZoomOutMap,
+} from "@mui/icons-material";
+import "./NavbarRight.css";
 
 export default function NavbarRight() {
-  return <div>NavbarRight</div>;
+  return (
+    <div>
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a href="" className="nav-link">
+            <Search />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="" className="nav-link">
+            <QuestionAnswer />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="" className="nav-link">
+            <NotificationsNone />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="" className="nav-link">
+            <ZoomOutMap />
+          </a>
+        </li>
+        <li className="nav-item">
+          <a href="" className="nav-link">
+            <Window />
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
