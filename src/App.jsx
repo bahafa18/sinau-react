4137935540ce5c359d868306d1956ab4de233ba7
Fundream18 <fandriydb@gmail.com>
2023-07-09 23:11:26 +0800

    sidebar-ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 // import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
-import Topbar from "./components/topbar/Topbar";
+import Main from "./components/main/Main";
 import Sidebar from "./components/sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
-      <Topbar />
       <div className="container">
         <Sidebar />
-        <div className="other">
-          other smentara <br />
-        </div>
+        <Main />
       </div>
     </div>
   );
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,26 +1,54 @@
-import { LineStyle, Timeline, TrendingUp } from "@mui/icons-material";
+import {
+  BookmarkBorder,
+  Campaign,
+  Dashboard,
+  Home,
+  Summarize,
+  WorkHistory,
+} from "@mui/icons-material";
 import "./Sidebar.css";
 
 export default function Sidebar() {
   return (
     <div className="sidebar">
-      <div className="sideBarWrapper">
-        <div className="sideBarMenu">
-          <h2 className="sideBarTitle">Dashboard</h2>
-          <ul className="sideBarList">
-            <li className="sideBarItem active">
-              <LineStyle className="sideBarIcon" />
-              Home
-            </li>
-            <li className="sideBarItem">
-              <Timeline className="sideBarIcon" />
-              Analytics
-            </li>
-            <li className="sideBarItem">
-              <TrendingUp className="sideBarIcon" />
-              Sales
-            </li>
-          </ul>
+      <div className="header">
+        <div className="list-item">
+          <a href="#">
+            <Home className="icon" />
+            <span className="desc-header">
+              baha<span>fa</span>
+            </span>
+          </a>
+        </div>
+        <div className="illustrations">
+          <img
+            src="https://img.freepik.com/free-vector/isometric-electrician-people-composition-with-linesmen-doing-various-works-vector-illustration_1284-30075.jpg?w=826&t=st=1688911483~exp=1688912083~hmac=c39024e945b89c55980b3f740085e7c2dd4b317c749058d057a4b175e81b96ae"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="menu">
+        <div className="list-item">
+          <a href="#">
+            <Dashboard className="icon" />
+            <span className="description">Dashboard</span>
+          </a>
+          <a href="#">
+            <Campaign className="icon" />
+            <span className="description">Laporan</span>
+          </a>
+          <a href="#">
+            <BookmarkBorder className="icon" />
+            <span className="description">Order</span>
+          </a>
+          <a href="#">
+            <WorkHistory className="icon" />
+            <span className="description">History</span>
+          </a>
+          <a href="#">
+            <Summarize className="icon" />
+            <span className="description">Report</span>
+          </a>
         </div>
       </div>
     </div>
